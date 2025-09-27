6422ee20856bfbc68bac660f9a3867cfd3e6da8b
renosang <renosang@gmail.com>
2025-09-27 09:23:23 +0700

    Fix Deploy vercel

--- src/pages/components/Header.tsx
+++ src/pages/components/Header.tsx
@@ -1,28 +1,16 @@
-import React, { useEffect} from 'react';
+import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import useAuthStore from '../../stores/useAuthStore';
 import './Header.css';
 
 function Header() {
-  const { user, logout } = useAuthStore((state) => ({
-    user: state.user,
-    logout: state.logout,
-  }));
-
-  console.log('Current user:', user); // Kiểm tra thông tin user
+  const { user, logout } = useAuthStore();
   const navigate = useNavigate();
 
   const handleLogout = () => {
     logout();
     navigate('/login');
   };
-  // const [userRole, setUserRole] = useState('');
-  useEffect(() => {
-const urlParams = new URLSearchParams(window.location.search);
-// const userRole = urlParams.get('userrole');
-// setUserRole(userRole || '');
-
-  }, [user]);
 
   return (
     <header className="app-header">
@@ -30,15 +18,20 @@ const urlParams = new URLSearchParams(window.location.search);
         <Link to="/dashboard" className="logo-link">
           <img src={process.env.PUBLIC_URL + '/logo.png'} alt="ThienTu" className="header-logo" />
         </Link>
+        <nav className="header-nav">
+          {/* Liên kết này chỉ hiển thị khi người dùng là admin */}
+          {user?.role === 'admin' && (
+            <Link to="/admin">Trang quản trị</Link>
+          )}
+        </nav>
       </div>
       <div className="header-right">
-        <span className="user-greeting">Xin chào, Chúc bạn làm việc hiệu quả ❤️</span>        
-        {/* Chỉ hiển thị nút này cho user thường */}
-        {/* {userRole === 'user' && ( */}
-             <button className="contribute-button" onClick={() => navigate('/dashboard/contribute')}>
-                Đóng góp Macro
-             </button>
-        {/* )} */}
+        {/* Lời chào được cá nhân hóa */}
+        <span className="user-greeting">Xin chào, Chúc bạn làm việc hiệu quả ❤️!</span>
+        
+          <button className="contribute-button" onClick={() => navigate('/dashboard/contribute')}>
+            Đóng góp Macro
+          </button>        
         <button className="logout-button" onClick={handleLogout}>
           Đăng xuất
         </button>
@@ -48,4 +41,3 @@ const urlParams = new URLSearchParams(window.location.search);
 }
 
 export default Header;
-
