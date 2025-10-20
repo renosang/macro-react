7704c89a01bc095d2d92ec1976f36d542014dc26
renosang <renosang@gmail.com>
2025-10-20 11:43:45 +0700

    Fix: Properly untrack all node_modules files

--- src/utils/slateUtils.ts
+++
@@ -1,6 +0,0 @@
-import { Descendant, Node } from 'slate';
-
-export const serializeSlate = (nodes: Descendant[]): string => {
-  if (!Array.isArray(nodes)) return '';
-  return nodes.map(n => Node.string(n)).join('\n');
-};
